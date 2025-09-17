aae7896ce3e940d1dd2dba12eead00f0825b761d
Nazar Zozulya <likeemangames@gmail.com>
2025-09-17 20:19:04 +0300

    add many fixes

--- krestiki-noliki/Program.cs
+++ krestiki-noliki/Program.cs
@@ -4,7 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[,] matrix = new int[3, 3]; // 0 -пуста клітина, 1 - клітина X, 2 - клітина O,
+
+        int[] matrix = new int[9];
+        // int[,] matrix = new int[3, 3]; // 0 -пуста клітина, 1 - клітина X, 2 - клітина O,
 
         bool is_game = true;
 
@@ -12,84 +14,89 @@ class Program
 
         while (is_game)
         {
-            ScreenDrawing(matrix);
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                // for (int j = 0; j < 1; j++)
+                // {
+                System.Console.Write(matrix[i] + "|");
+                // }
+                switch (i)
+                {
+                    case 2:
+                        System.Console.WriteLine();
+                        break;
+                    case 5:
+                        System.Console.WriteLine();
+                        break;
+                    case 8:
+                        System.Console.WriteLine();
+                        break;
+                    
+                }
 
-            System.Console.WriteLine($"{(whos_move ? 'X' : 'O')} хід, ходити ось так '3 3' сначала висота потім ширина ");
-            string[] MoveCors = Console.ReadLine().Split(" ");
+                // System.Console.WriteLine();
+            }
 
-            int MoveResult = matrix[Convert.ToInt32(MoveCors[0]) - 1, Convert.ToInt32(MoveCors[1]) - 1];
+            System.Console.WriteLine($"{(whos_move ? 'X' : 'O')} хід, напишіть номер ячейки від 1 до 9 ");
+            string MoveCords = Console.ReadLine();
+
+            int MoveResult = matrix[Convert.ToInt32(MoveCords) -1];
 
             if (MoveResult != 0)
             {
-                if (MoveResult == 1 && whos_move == true)
-                {
-                    System.Console.WriteLine("Ця клітина вже зайнята вами");
-                }
-                if (MoveResult == 2 && whos_move == true)
-                {
-                    System.Console.WriteLine("Ця клітина вже зайнята ворогом");
-                }
-
-                if (MoveResult == 2 && whos_move == false)
-                {
-                    System.Console.WriteLine("Ця клітина вже зайнята вами");
-                }
-                if (MoveResult == 1 && whos_move == false)
-                {
-                    System.Console.WriteLine("Ця клітина вже зайнята ворогом");
-                }
+                System.Console.WriteLine("Ця ячейка вже зайнята");
             }
             else
             {
-                matrix[Convert.ToInt32(MoveCors[0]) - 1, Convert.ToInt32(MoveCors[1]) - 1] = whos_move ? 1 : 2;
+                matrix[Convert.ToInt32(MoveCords) - 1] = whos_move ? 1 : 2;
 
                 System.Console.WriteLine(1);
 
                 // Для Х
 
-                if (matrix[0, 0] == 1 && matrix[0, 1] == 1 && matrix[0, 2] == 1)
+                if (matrix[0] == 1 && matrix[1] == 1 && matrix[2] == 1)
                 {
                     System.Console.WriteLine("X вийграв");
                     is_game = false;
                 }
 
-                if (matrix[1, 0] == 1 && matrix[1, 1] == 1 && matrix[1, 2] == 1)
+                else if (matrix[3] == 1 && matrix[4] == 1 && matrix[5] == 1)
                 {
                     System.Console.WriteLine("X вийграв");
                     is_game = false;
                 }
 
-                if (matrix[2, 0] == 1 && matrix[2, 1] == 1 && matrix[2, 2] == 1)
+                else if (matrix[6] == 1 && matrix[7] == 1 && matrix[8] == 1)
                 {
                     System.Console.WriteLine("X вийграв");
                     is_game = false;
                 }
 
-                if (matrix[0, 0] == 1 && matrix[1, 0] == 1 && matrix[2, 0] == 1)
+                else if (matrix[0] == 1 && matrix[3] == 1 && matrix[6] == 1)
                 {
                     System.Console.WriteLine("X вийграв");
                     is_game = false;
                 }
 
-                if (matrix[0, 1] == 1 && matrix[1, 1] == 1 && matrix[2, 1] == 1)
+                else if (matrix[1] == 1 && matrix[4] == 1 && matrix[7] == 1)
                 {
                     System.Console.WriteLine("X вийграв");
                     is_game = false;
                 }
 
-                if (matrix[0, 1] == 1 && matrix[1, 1] == 1 && matrix[2, 1] == 1)
+                else if (matrix[2] == 1 && matrix[5] == 1 && matrix[8] == 1)
                 {
                     System.Console.WriteLine("X вийграв");
                     is_game = false;
                 }
 
-                if (matrix[0, 0] == 1 && matrix[1, 1] == 1 && matrix[2, 2] == 1)
+                else if (matrix[0] == 1 && matrix[4] == 1 && matrix[8] == 1)
                 {
                     System.Console.WriteLine("X вийграв");
                     is_game = false;
                 }
 
-                if (matrix[0, 2] == 1 && matrix[1, 1] == 1 && matrix[2, 0] == 1)
+                else if (matrix[2] == 1 && matrix[4] == 1 && matrix[6] == 1)
                 {
                     System.Console.WriteLine("X вийграв");
                     is_game = false;
@@ -98,49 +105,50 @@ class Program
 
 
                 // для О
-                if (matrix[0, 0] == 2 && matrix[0, 1] == 2 && matrix[0, 2] == 2)
+
+                if (matrix[0] == 2 && matrix[1] == 2 && matrix[2] == 2)
                 {
                     System.Console.WriteLine("O вийграв");
                     is_game = false;
                 }
 
-                if (matrix[1, 0] == 2 && matrix[1, 1] == 2 && matrix[1, 2] == 2)
+                else if (matrix[3] == 2 && matrix[4] == 2 && matrix[5] == 2)
                 {
                     System.Console.WriteLine("O вийграв");
                     is_game = false;
                 }
 
-                if (matrix[2, 0] == 2 && matrix[2, 1] == 2 && matrix[2, 2] == 2)
+                else if (matrix[6] == 2 && matrix[7] == 2 && matrix[8] == 2)
                 {
                     System.Console.WriteLine("O вийграв");
                     is_game = false;
                 }
 
-                if (matrix[0, 0] == 2 && matrix[1, 0] == 2 && matrix[2, 0] == 2)
+                else if (matrix[0] == 2 && matrix[3] == 2 && matrix[6] == 2)
                 {
                     System.Console.WriteLine("O вийграв");
                     is_game = false;
                 }
 
-                if (matrix[0, 1] == 2 && matrix[1, 1] == 2 && matrix[2, 1] == 2)
+                else if (matrix[1] == 2 && matrix[4] == 2 && matrix[7] == 2)
                 {
                     System.Console.WriteLine("O вийграв");
                     is_game = false;
                 }
 
-                if (matrix[0, 1] == 2 && matrix[1, 1] == 2 && matrix[2, 1] == 2)
+                else if (matrix[2] == 2 && matrix[5] == 2 && matrix[8] == 2)
                 {
                     System.Console.WriteLine("O вийграв");
                     is_game = false;
                 }
 
-                if (matrix[0, 0] == 2 && matrix[1, 1] == 2 && matrix[2, 2] == 2)
+                else if (matrix[0] == 2 && matrix[4] == 2 && matrix[8] == 2)
                 {
                     System.Console.WriteLine("O вийграв");
                     is_game = false;
                 }
 
-                if (matrix[0, 2] == 2 && matrix[1, 1] == 2 && matrix[2, 0] == 2)
+                else if (matrix[2] == 2 && matrix[4] == 2 && matrix[6] == 2)
                 {
                     System.Console.WriteLine("O вийграв");
                     is_game = false;
@@ -152,13 +160,10 @@ class Program
             bool hasZero = false;
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                if (matrix[i] == 0)
                 {
-                    if (matrix[i, j] == 0)
-                    {
-                        hasZero = true;
-                        break;
-                    }
+                    hasZero = true;
+                    break;
                 }
                 if (hasZero) break;
             }
@@ -173,16 +178,5 @@ class Program
 
         }
 
-    }
-    static void ScreenDrawing(int[,] matrix)
-    {
-        for (int i = 0; i < matrix.GetLength(0); i++)
-        {
-            for (int j = 0; j < matrix.GetLength(1); j++)
-            {
-                System.Console.Write(matrix[i, j] + "|");
-            }
-            System.Console.WriteLine();
-        }
     }
 }
